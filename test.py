72de3d0aff19b50dc0b7c43488ab7ff746c355f2
ThnhCong <shin141106@gmail.com>
2025-10-27 00:39:59 +0700

    oke final

--- test.py
+++ test.py
@@ -1,22 +1,24 @@
 from collections import Counter
 import heapq
 
-from pkg_resources import non_empty_lines
-
 with open("input.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
-print("Nội dung file:")
+print(" Nội dung file:")
 print(text)
 
 freq = Counter(text)
-
-print("\n frequence of character:")
-for char, count in freq.item():
-    print(f"'{char}': {count}")
+print("\n📊 Tần suất xuất hiện ký tự:")
+for char, count in freq.items():
+    if char == "\n":
+        print("'\\n':", count)
+    elif char == " ":
+        print("'space':", count)
+    else:
+        print(f"'{char}': {count}")
 
 class Node:
-    def __init__(self, char, freg):
+    def __init__(self, char, freq):
         self.char = char
         self.freq = freq
         self.left = None
@@ -25,7 +27,8 @@ class Node:
     def __lt__(self, other):
         return self.freq < other.freq
 
-heap = [Node(char, f) for char, f in freq.item()]
+
+heap = [Node(char, f) for char, f in freq.items()]
 heapq.heapify(heap)
 
 while len(heap) > 1:
@@ -37,3 +40,59 @@ while len(heap) > 1:
     heapq.heappush(heap, merged)
 
 root = heap[0]
+
+codes = {}
+
+def generate_codes(node, current_code=""):
+    if node is None:
+        return
+    if node.char is not None:
+        codes[node.char] = current_code
+        return
+    generate_codes(node.left, current_code + "0")
+    generate_codes(node.right, current_code + "1")
+
+generate_codes(root)
+
+print("\n Bảng mã Huffman:")
+for char, code in codes.items():
+    if char == "\n":
+        print("'\\n':", code)
+    elif char == " ":
+        print("'space':", code)
+    else:
+        print(f"'{char}': {code}")
+
+encoded_text = "".join(codes[ch] for ch in text)
+print("\n Chuỗi bit mã hóa:")
+print(encoded_text)
+
+padding = 8 - len(encoded_text) % 8
+encoded_text += "0" * padding
+padding_info = "{0:08b}".format(padding)
+binary_data = padding_info + encoded_text
+
+b = bytearray()
+for i in range(0, len(binary_data), 8):
+    byte = binary_data[i:i+8]
+    b.append(int(byte, 2))
+
+with open("output.bin", "wb") as f:
+    f.write(bytes(b))
+
+print(f"\n Đã lưu file nhị phân: output.bin ({len(b)} bytes)")
+
+def decode_text(encoded_text, root):
+    decoded = ""
+    node = root
+    for bit in encoded_text:
+        node = node.left if bit == "0" else node.right
+        if node.char is not None:
+            decoded += node.char
+            node = root
+    return decoded
+
+decoded_text = decode_text(encoded_text[:-padding], root)
+
+print("\n Giải mã lại văn bản:")
+print(decoded_text)
